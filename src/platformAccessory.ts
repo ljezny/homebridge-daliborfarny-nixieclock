426d324c3c65e56a1543c5325ee111a61bacf005
Lukas Jezny <ljezny@gmail.com>
2022-03-21 22:18:14 +0100

    Underlight color kickoff

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -4,7 +4,7 @@ import { NixieClocksHomebridgePlatform } from './platform';
 
 export class NixieClocksPlatformAccessory {
   private service: Service;
-
+  private underlightService?: Service;
   constructor(
     private readonly platform: NixieClocksHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
@@ -16,16 +16,38 @@ export class NixieClocksPlatformAccessory {
       .setCharacteristic(this.platform.Characteristic.Model, accessory.context.device.product_name)
       .setCharacteristic(this.platform.Characteristic.SerialNumber, accessory.context.device.id);
 
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
+    this.service = this.accessory.getService(this.platform.Service.Lightbulb)
+      || this.accessory.addService(this.platform.Service.Lightbulb, 'Tubes', 'Tubes');
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.product_name);
 
     this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+      .onSet(this.setOn.bind(this))
+      .onGet(this.getOn.bind(this));
 
     this.service.getCharacteristic(this.platform.Characteristic.Brightness)
       .onSet(this.setBrightness.bind(this))
-      .onGet(this.getBrightness.bind(this));      // SET - bind to the 'setBrightness` method below
+      .onGet(this.getBrightness.bind(this));
+
+    if(this.supportsUnderLight()) {
+      this.underlightService = this.accessory.getService('Underlight')
+        || this.accessory.addService(this.platform.Service.Lightbulb, 'Underlight', 'Underlight');
+
+      this.underlightService.getCharacteristic(this.platform.Characteristic.On)
+        .onSet(this.setUnderlightOn.bind(this))
+        .onGet(this.getUnderlightOn.bind(this));
+
+      this.underlightService.getCharacteristic(this.platform.Characteristic.Hue)
+        .onSet(this.setUnderlightHue.bind(this))
+        .onGet(this.getUnderlightHue.bind(this));
+
+      this.underlightService.getCharacteristic(this.platform.Characteristic.Saturation)
+        .onSet(this.setUnderlightHue.bind(this))
+        .onGet(this.getUnderlightHue.bind(this));
+    }
+  }
+
+  supportsUnderLight(): boolean {
+    return this.accessory.context.device.id_product === '2';
   }
 
   async setOn(value: CharacteristicValue) {
@@ -40,6 +62,32 @@ export class NixieClocksPlatformAccessory {
       return value.data.value > 0;
     }
     throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+  }
+
+  async setUnderlightOn(value: CharacteristicValue) {
+    if(value as boolean) {
+      await this.platform.dfApi.setDeviceParameter(this.accessory.context.device.id, 'underlightsegments', 6);
+      await this.platform.dfApi.setDeviceParameter(this.accessory.context.device.id, 'underlightmode', 2);
+    } else {
+      await this.platform.dfApi.setDeviceParameter(this.accessory.context.device.id, 'underlightsegments', 1);
+      await this.platform.dfApi.setDeviceParameter(this.accessory.context.device.id, 'underlightmode', 1);
+    }
+  }
+
+  async getUnderlightOn(): Promise<CharacteristicValue> {
+    const value = await this.platform.dfApi.getDeviceParameter(this.accessory.context.device.id, 'underlightsegments');
+    if(value.ok) {
+      return value.data.value > 0;
+    }
+    throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+  }
+
+  async setUnderlightHue(value: CharacteristicValue) {
+    this.platform.log.debug('Set Characteristic Hue -> ', value);
+  }
+
+  async getUnderlightHue(): Promise<CharacteristicValue> {
+    return 45;
   }
 
   async setBrightness(value: CharacteristicValue) {
